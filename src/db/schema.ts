aad85e193148c967a7164e5ae7174be3e56244d8
Sarmad rafique <sarmadrafique040@gmail.com>
2024-08-05 15:46:27 +0500

    Removed userId from drizzle schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -4,5 +4,4 @@ export const posts = pgTable('posts', {
     id: serial('id').primaryKey(),
     title: varchar('title', { length: 255 }).notNull(),
     description: text('description').notNull(),
-    userId: uuid('userId').notNull()
 })
